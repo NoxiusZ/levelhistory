b059d48703d88a940dfbe4523a73a7747887efc4
NoxiusZ <90790107+NoxiusZ@users.noreply.github.com>
2026-07-24 17:35:22 -0300

    updated to geode v5

--- src/GeodeNetwork.cpp
+++ src/GeodeNetwork.cpp
@@ -27,42 +27,26 @@ void GeodeNetwork::send() {
     geode::utils::web::WebTask task;
     
     if (_method == MGet) {
-        task = req.get(_url);
+        _listener.spawn(req.get(_url), [this](geode::utils::web::WebResponse res) {
+            this->_data = res.string().unwrapOr("Not a string);
+
+            if (res.ok() && this->_onOk != nullptr) {
+                this->_onOk(this);
+
+                return;
+            }
+
+            if (!res.ok() && this->_onError != nullptr) {
+                this->_onError(this);
+
+                return;
+            }
+        });
     }
+}
 
     _listener.setFilter(task);
 }
 
 GeodeNetwork::GeodeNetwork() {
-    setupListener();
-}
-
-void GeodeNetwork::setupListener() {
-    _listener.bind([this] (geode::utils::web::WebTask::Event* e) {
-        if (geode::utils::web::WebResponse* res = e->getValue()) {
-            this->_data = res->string().unwrapOr("Not a string");
-
-            // log::info("_data = {}", this->_data);
-
-            if (res->ok() && this->_onOk != nullptr) {
-                this->_onOk(this); 
-
-                return;
-            }
-
-            if (!res->ok() && this->_onError != nullptr) {
-                this->_onError(this); 
-
-                return;
-            }
-        } else if (e->isCancelled()) {
-            this->_data = "Error: cancelled";
-
-            if (this->_onError != nullptr) {
-                this->_onError(this); 
-
-                return;
-            }
-        }
-    });
 }
